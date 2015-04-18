db4a6e6a1aec24672a53269a379d7244315ea17b
James Kwapien <james.m.kwapien@wmich.edu>
2015-04-17 22:37:50 -0400

    loses

--- db/migrate/20150418023254_fix_column_name.rb
+++ db/migrate/20150418023254_fix_column_name.rb
@@ -1,5 +1,7 @@
 class FixColumnName < ActiveRecord::Migration
   def change
     rename_column :Teams, :loses, :losses
+    rename_column :Teams, :overTimeLoses, :overTimeLosses
+    rename_column :Teams, :shootOutLoses, :shootOutLosses
   end
 end
